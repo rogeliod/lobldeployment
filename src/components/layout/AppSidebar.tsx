53b146f4db41546f12138c7ec02126186e14d95b
Sandip Satyal <sandipsatyal25@gmail.com>
2025-09-07 13:02:36 +0545

    add: shadcn component for app sidebar

---
+++ src/components/layout/AppSidebar.tsx
@@ -0,0 +1,51 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+import { useDnd } from "@/context/DragAndDropContext";
+
+import { typeOfNodes } from "@/data/nodes";
+
+function AppSidebar() {
+  const { setType } = useDnd();
+
+  const onDragInit = (e: React.DragEvent<HTMLDivElement>, nodeType: string) => {
+    setType(nodeType);
+    e.dataTransfer.effectAllowed = "move";
+  };
+
+  return (
+    <Sidebar>
+      <SidebarHeader className="font-semibold">Components</SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {typeOfNodes.map((nodes) => (
+                <SidebarMenuItem key={nodes.label}>
+                  <SidebarMenuButton asChild>
+                    <div
+                      draggable
+                      onDragStart={(e) => onDragInit(e, nodes.type)}
+                    >
+                      {nodes.label}
+                    </div>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
+
+export default AppSidebar;
